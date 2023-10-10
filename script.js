dc2f12441da2756c9e3d9d1f576fe33572760740
CWu101 <132257510+CWu101@users.noreply.github.com>
2023-10-10 10:18:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function calculate() {
-    const input1 = document.getElementById('input1').value;
-    const input2 = document.getElementById('input2').value;
-    const result = parseFloat(input1) + parseFloat(input2);
-    document.getElementById('output').value = result;
+const API_ENDPOINT = "https://api.openai.com/v1/engines/text-davinci-002/completions";
+
+function fetchResponse() {
+    const API_KEY = document.getElementById('apiInput').value;
+    const userInput = document.getElementById('userInput').value;
+
+    if (userInput === "") {
+        return;
+    }
+
+    const headers = new Headers({
+        'Authorization': `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json'
+    });
+
+    const body = JSON.stringify({
+        prompt: userInput,
+        max_tokens: 50
+    });
+
+    fetch(API_ENDPOINT, {
+        method: 'POST',
+        headers: headers,
+        body: body
+    })
+    .then(response => response.json())
+    .then(data => {
+        const ans = data.choices[0].text.trim();
+        document.getElementById('response').innerText = ans;
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
 }
